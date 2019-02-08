47221c0eb094ae1cd8ea07f1a4843cc77ce61858
Anton <devil675@mail.ru>
2019-02-08 20:50:42 +0700

    first changes

--- src/main/java/com/axmor/Main.java
+++ src/main/java/com/axmor/Main.java
@@ -3,8 +3,8 @@ package com.axmor;
 import spark.Request;
 import spark.Response;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
+import static spark.Spark.*;
+import static spark.debug.DebugScreen.*;
 
 /**
  * Application entry point
@@ -12,6 +12,10 @@ import static spark.Spark.port;
 public class Main {
     public static void main(String[] args) {
         port(80);
+        staticFiles.location("/public");
+        staticFiles.expireTime(600L);
+        enableDebugScreen();
+
         get("/", (Request req, Response res) ->
                 "<html><body><h1>Hello, world!</h1></body></html>");
     }
